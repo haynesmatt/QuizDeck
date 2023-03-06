77d0942eb403861086eeaf88e906b75253a11687
Matt <matthaynes412@gmail.com>
2023-03-06 15:16:08 -0500

    create header & card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import './Card.css'
+import React from 'react';
+
+const Card = () => {
+
+    return (
+        <div className='Card'>
+            <p>test</p>
+        </div>
+    )
+
+}
+
+export default Card;
